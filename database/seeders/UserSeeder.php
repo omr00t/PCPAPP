d3ddd551f4fca17f3a93d9ded3effaf5a13233b5
omr00t <76473301+omr00t@users.noreply.github.com>
2021-12-31 14:38:53 +0400

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,41 +16,43 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'username' => "admin",
-            'first_name' => "Yousuf",
-            'last_name' => "Alhajri",
-            'email' => "joe@omani.cloud",
-            'password' => Hash::make('password'),
-            'gender' => "Male",
-            'is_verified' => 1,
-            'role' => "admin",
-            'about' => "I'm the best admin, for sure.",
-            'status' => 1,
-        ]);
-        DB::table('users')->insert([
-            'username' => "khalid",
-            'first_name' => "Khalid",
-            'last_name' => "Alhajri",
-            'email' => "khalid@omani.cloud",
-            'password' => Hash::make('hello123'),
-            'gender' => "Male",
-            'is_verified' => 1,
-            'role' => "user",
-            'about' => "A simple user.",
-            'status' => 1,
-        ]);
-        DB::table('users')->insert([
-            'username' => "rashid",
-            'first_name' => "Rashid",
-            'last_name' => "Alhajri",
-            'email' => "rashid@omani.cloud",
-            'password' => Hash::make('rashid123'),
-            'gender' => "Male",
-            'is_verified' => 1,
-            'role' => "manager",
-            'about' => "A manager.",
-            'status' => 1,
-        ]);
+        if(empty(DB::table('users')->get())){
+            DB::table('users')->insert([
+                'username' => "admin",
+                'first_name' => "Yousuf",
+                'last_name' => "Alhajri",
+                'email' => "joe@omani.cloud",
+                'password' => Hash::make('password'),
+                'gender' => "Male",
+                'is_verified' => 1,
+                'role' => "admin",
+                'about' => "I'm the best admin, for sure.",
+                'status' => 1,
+            ]);
+            DB::table('users')->insert([
+                'username' => "khalid",
+                'first_name' => "Khalid",
+                'last_name' => "Alhajri",
+                'email' => "khalid@omani.cloud",
+                'password' => Hash::make('hello123'),
+                'gender' => "Male",
+                'is_verified' => 1,
+                'role' => "user",
+                'about' => "A simple user.",
+                'status' => 1,
+            ]);
+            DB::table('users')->insert([
+                'username' => "rashid",
+                'first_name' => "Rashid",
+                'last_name' => "Alhajri",
+                'email' => "rashid@omani.cloud",
+                'password' => Hash::make('rashid123'),
+                'gender' => "Male",
+                'is_verified' => 1,
+                'role' => "manager",
+                'about' => "A manager.",
+                'status' => 1,
+            ]);
+        }
     }
 }
